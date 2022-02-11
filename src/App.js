0d1a40017340838baca22db534210e3f4ce23d41
Fom Nanpan Wilson <79115812+fomnanpan12@users.noreply.github.com>
2022-02-11 11:24:28 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 // import { render } from "react-dom"; 
 // import { Component } from "react/cjs/react.production.min";
 import Cardlist from './Cardlist';
-import {robots} from './robots';
+// import {robots} from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
+import Scroll from './Scroll';
 
 
 // const state ={
@@ -16,10 +17,18 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield:'',
         }
     }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> response.json())
+            .then(users =>this.setState({ robots: users}));
+    }
+    
+
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
         
@@ -29,13 +38,20 @@ class App extends Component {
         const filterdRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className='tc'>
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Cardlist robots={filterdRobots}/>
-            </div>
-        );
+        if(this.state.robots.length === 0) {
+            return <h1 className="tc">Loading</h1>
+        } else {
+
+                return (
+                    <div className='tc'>
+                        <h1 className="f1">RoboFriends</h1>
+                        <SearchBox searchChange={this.onSearchChange}/>
+                        <Scroll>
+                            <Cardlist robots={filterdRobots}/>
+                        </Scroll>
+                    </div>
+                );
+        }
     }
 }
 
